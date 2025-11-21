59375eb28932a459d0398cb86769f8fbb3a3fc61
Adri <adricelluc@gmail.com>
2025-11-21 15:28:07 -0500

    indexhtml

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'main.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 //API middleware route /api/add?a=2&b=3
